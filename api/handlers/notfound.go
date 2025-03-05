a9eaf3c6998e7dc4d722ae86b4a3c29815e17a77
muhammet <muhammetaticitr@gmail.com>
2025-03-05 21:49:09 +0300

    method not found handler implemented

--- api/handlers/notfound.go
+++ api/handlers/notfound.go
@@ -1,9 +1,17 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/aticish/log-service/internal"
+	"github.com/gofiber/fiber/v2"
+)
 
 func NotFound(c *fiber.Ctx) error {
 	c.Context().SetContentType("application/json")
 	c.SendStatus(fiber.StatusNotFound)
-	return nil
+	return c.JSON(&internal.Response{
+		Code:    fiber.StatusNotFound,
+		Status:  "error",
+		Message: "Not Found",
+		Data:    nil,
+	})
 }
