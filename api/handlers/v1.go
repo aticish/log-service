5f43e99311dbe6a1bdf483a5859263e9d21a2924
muhammet <muhammetaticitr@gmail.com>
2025-03-06 13:30:03 +0300

    parse request

--- api/handlers/v1.go
+++ api/handlers/v1.go
@@ -1,8 +1,21 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/aticish/log-service/internal"
+	"github.com/gofiber/fiber/v2"
+)
 
 func VersionOne(c *fiber.Ctx) error {
 	c.Context().SetContentType("application/json")
+
+	// Read request body
+	var request *internal.Request
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(&internal.Response{
+			Code:    fiber.StatusBadRequest,
+			Status:  "error",
+			Message: internal.ErrorInvalidJSON.Error(),
+		})
+	}
 	return nil
 }
--- api/internal/errors.go
+++ api/internal/errors.go
@@ -3,5 +3,8 @@ package internal
 import "errors"
 
 var (
-	ErrorPageNotFound = errors.New("page not found")
+	ErrorPageNotFound     = errors.New("page not found")
+	ErrorInvalidJSON      = errors.New("request body is not a valid json")
+	ErrorInvalidToken     = errors.New("bearer token is invalid")
+	ErrorInvalidLogMethod = errors.New("invalid log method. only 'read' and 'write' accepted")
 )
